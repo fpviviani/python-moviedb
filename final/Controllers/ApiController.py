a560f915451ed100c7d9c775f9093da9b459d792
fpviviani <fpviviani@gmail.com>
2020-10-18 18:57:29 -0300

    Correção em comentários

--- final/Controllers/ApiController.py
+++ final/Controllers/ApiController.py
@@ -4,8 +4,8 @@ class ApiController():
     def __init__(self, mainController):  
         self.mainController = mainController
         self.apiKey = "619e50c9f8b2c6aacab7bbc2db6c67a7"
-        self.searchMovie()
-        self.getGenres()
+        # self.searchMovie()
+        # self.getGenres()
 
     def searchMovie(self):
         query = str(input('Digite a string pela qual deseja pesquisar: '))
@@ -15,13 +15,13 @@ class ApiController():
         response = requests.get(requestUrl)
         # Separa os dados apenas do primeiro filme encontrado
         results = json.loads(response.content)["results"][0]
-        # print (results)
+        print (results)
         
     def getGenres(self):
         # Monta a url da request que será feita
         requestUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key="+self.apiKey+"&language=en-US"
         # Pega o conteudo da resposta
         response = requests.get(requestUrl)
-        # Separa os dados apenas do primeiro filme encontrado
+        # Separa os dados dos generos cadastrados
         results = json.loads(response.content)["genres"]
         print (results)
