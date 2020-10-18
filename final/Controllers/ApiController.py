46184dc5b7c3da57590226aadcc183a65b4eb9d6
fpviviani <fpviviani@gmail.com>
2020-10-18 18:55:05 -0300

    Método para pegar os generos

--- final/Controllers/ApiController.py
+++ final/Controllers/ApiController.py
@@ -5,6 +5,7 @@ class ApiController():
         self.mainController = mainController
         self.apiKey = "619e50c9f8b2c6aacab7bbc2db6c67a7"
         self.searchMovie()
+        self.getGenres()
 
     def searchMovie(self):
         query = str(input('Digite a string pela qual deseja pesquisar: '))
@@ -14,5 +15,13 @@ class ApiController():
         response = requests.get(requestUrl)
         # Separa os dados apenas do primeiro filme encontrado
         results = json.loads(response.content)["results"][0]
-        print (results)
+        # print (results)
         
+    def getGenres(self):
+        # Monta a url da request que será feita
+        requestUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key="+self.apiKey+"&language=en-US"
+        # Pega o conteudo da resposta
+        response = requests.get(requestUrl)
+        # Separa os dados apenas do primeiro filme encontrado
+        results = json.loads(response.content)["genres"]
+        print (results)
